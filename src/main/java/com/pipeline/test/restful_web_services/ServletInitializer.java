b78e8f574a22ec70626a195668495727c3ef6832
prasadspatil <prasadpatil002@gmail.com>
2024-12-25 17:52:54 +0530

    add project files

---
+++ src/main/java/com/pipeline/test/restful_web_services/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.pipeline.test.restful_web_services;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestfulWebServicesApplication.class);
+	}
+
+}
